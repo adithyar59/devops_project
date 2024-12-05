14ee3620d75ab89ac99bd92ec60940c12fdd4d46
adithyar59 <adithyar0509@gmail.com>
2024-12-05 21:38:11 +0530

    final code changed

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
+const inputBox =document.getElementById("input-box");
+const listContainer =document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
 
-    if (taskInput.value === "") return;
-
-    const task = document.createElement("li");
-    task.innerHTML = `${taskInput.value} <button onclick="deleteTask(this)">Delete</button>`;
-    taskList.appendChild(task);
-
-    taskInput.value = "";
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
 }
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
 
-function deleteTask(button) {
-    button.parentElement.remove();
+    }
+}, false);
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML );
 }
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem(data);
+}
+showTask();
